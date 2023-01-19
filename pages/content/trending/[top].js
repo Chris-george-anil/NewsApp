56e1a8a7f7299a092c386ddcfdec1481cf0e1111
Chris-george-anil <chrisgeorgeanil@gmail.com>
2023-01-19 19:16:57 +0530

    Static fix

--- pages/content/trending/[top].js
+++ pages/content/trending/[top].js
@@ -13,23 +13,23 @@ const Trending = ({ data, title }) => {
 
 export default Trending;
 
-export async function getStaticPaths() {
-    const arr = ["business", "entertainment", "general", "health", "science", "sports", "technology"];
-    const allPaths = arr?.map((a) => {
-        return {
-            params: {
-                top: a
-            }
-        }
-    });
+// export async function getStaticPaths() {
+//     const arr = ["business", "entertainment", "general", "health", "science", "sports", "technology"];
+//     const allPaths = arr?.map((a) => {
+//         return {
+//             params: {
+//                 top: a
+//             }
+//         }
+//     });
 
-    return {
-        paths: allPaths, fallback: false
-    }
-}
+//     return {
+//         paths: allPaths, fallback: false
+//     }
+// }
 
-export async function getStaticProps(context) {
-    const topic = context.params.top;
+export async function getServerSideProps(context) {
+    const topic = context.query.top;
     console.log(context.query);
     const res = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${topic}&apiKey=${process.env.API_KEY}`);
     const data = await res.json();
