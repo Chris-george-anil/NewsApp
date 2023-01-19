a501bdfb6728a3723507c0fba72f881f3000d273
Chris-george-anil <chrisgeorgeanil@gmail.com>
2023-01-19 12:42:59 +0530

    firtstpush

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,5 @@
-import Mainlayout from '../src/components/layout/mainlayout'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Mainlayout>
-        <Component {...pageProps} />
-
-      </Mainlayout></>
-  )
+  return <Component {...pageProps} />
 }
--- src/components/footer/footer.jsx
+++
@@ -1,8 +0,0 @@
-export const Footer = () => {
-    return (
-        <footer>
-            <p>2022 NewsWeb  Made in India</p>
-
-        </footer>
-    )
-}
