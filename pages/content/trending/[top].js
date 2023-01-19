f9088f13545f87a66fbba99a4310e44775ee1e61
Chris-george-anil <chrisgeorgeanil@gmail.com>
2023-01-19 19:09:44 +0530

    Static Paths

--- pages/content/trending/[top].js
+++ pages/content/trending/[top].js
@@ -15,7 +15,7 @@ export default Trending;
 
 export async function getStaticPaths() {
     const arr = ["business", "entertainment", "general", "health", "science", "sports", "technology"];
-    const allPaths = arr.map((a) => {
+    const allPaths = arr?.map((a) => {
         return {
             params: {
                 top: a
@@ -30,7 +30,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
     const topic = context.params.top;
-    // console.log(topic);
+    console.log(context.query);
     const res = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${topic}&apiKey=${process.env.API_KEY}`);
     const data = await res.json();
     return { props: { data: data, title: topic } }
